a50bc441211e04f1a1d73e822e082e984d81d54b
Setric muholo <muholosetric@gmail.com>
2022-11-09 18:41:03 +0000

    shell project

---
+++ README.md
@@ -0,0 +1,1 @@
+this a repository on simple shell project made by ALX curriculum it is a team project.
---
+++ shell.h
@@ -0,0 +1,51 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
+
+/**
+ * struct list_s - A new struct type defining a linked list.
+ * @dir: A directory path.
+ * @next: A pointer to another struct list_s.
+ */
+typedef struct list_s
+{
+  char *dir;
+  struct list_s *next;
+} list_t;
+
+/**
+ * struct builtin_s - A new struct type defining builtin commands.
+ * @name: The name of the builtin command.
+ * @f: A function pointer to the builtin command's function.
+ */
+typedef struct builtin_s
+{
+  char *name;
+  int (*f)(char **argv, char **front);
+} builtin_t;
+
+/**
+ * struct alias_s - A new struct defining aliases.
+ * @name: The name of the alias.
+ * @value: The value of the alias.
+ * @next: A pointer to another struct alias_s.
+ */
+typedef struct alias_s
+{
+  char *name;
+  char *value;
+  struct alias_s *next;
+} alias_t;
